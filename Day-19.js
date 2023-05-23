ae8a36878304a22fd0eadaafcc17f1e4602eb662
Swaraj Jadhav <70018714+swaraj54@users.noreply.github.com>
2023-05-23 08:26:08 +0530

    Updates

--- Day-19.js
+++ Day-19.js
@@ -1,13 +1,13 @@
-// Datatypes -
-// String - text -"I-rise" 'Institute' `Helllo world` "7865"
-// Number - number - 324 8765 4 0 87.87 765 
-// Boolean - true false
-// undefined - 
+Datatypes -
+String - text -"I-rise" 'Institute' `Helllo world` "7865"
+Number - number - 324 8765 4 0 87.87 765 
+Boolean - true false
+undefined - 
 
-// varibles - can store - data
-// var myAge = "I-rise";
-// var myAge = 876544;
-// var myAge = false;
+varibles - can store - data
+var myAge = "I-se";
+var myAge = 876544;
+var myAge = false;
 var myAge;
 console.log(myAge, "- my age")
 console.log(typeof(myAge), "- datatype")
---
+++ Day-20.js
@@ -0,0 +1,38 @@
+var myName = 'Irise'; // decleraton - assign
+var surName = 'Institute';
+var myLocation = "Pune";
+var myAge = 5;
+var myExperties = 'Development';
+// console.log(myName,surName,myLocation,myAge,myExperties);
+
+
+// var firstNumber = 10;
+// var secondNumber = 20;
+// console.log(firstNumber + secondNumber)
+// console.log(firstNumber - secondNumber)
+// console.log(firstNumber * secondNumber)
+// console.log(firstNumber / secondNumber)
+// = assign 
+// == comparison with only data
+// === comparison with dataTypes and data, both
+// var number = 100;
+// console.log(number == '100' )
+// console.log(number === '100' )
+
+// Conditionl Statement 
+// 1. if else 
+// 2. switch
+var number = 100;
+// console.log(number <= 100)
+if (number >= 100) {
+    console.log("nUMBER IS GRETER THAN or equal 100")
+} else if (number > 80) {
+    console.log("nUMBER IS GRETER THAN 80")
+} else if (number > 60) {
+    console.log("nUMBER IS GRETER THAN 80")
+} else if (number > 40) {
+    console.log("nUMBER IS GRETER THAN 80")
+} else {
+    console.log('Nummber is not greater than 80 and 100')
+}
+
